97919373cdc9dfdc5eca437ebb0f9d5fe2fa0dc9
kev-th <kevinkthaw@gmail.com>
2018-04-28 13:15:59 -0400

    add IBM Cloud Watson Visual Recognation

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ const app = express();
 const router = express.Router();
 const port = process.env.PORT || 5000;
 const path = require('path');
+const fs = require('fs');
 
-var html_dir = './views/'
+//IBM Cloud Watson Visual Recognation
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+
+var visualRecognition = new VisualRecognitionV3({
+    version_date: '2018-03-19',
+    api_key: "63e90ae817c302706cc1a4b0ad348b3c7643f78b",
+    headers: {
+        'X-Watson-Learning-Opt-Out': 'true'
+    }
+  });
+
+var images_file = fs.createReadStream('./clothes.png');
+var classifier_ids = ["default"];
+
+var params = {
+    images_file: images_file,
+    classifier_ids: classifier_ids
+};
+
+visualRecognition.classify(params, (err, res)=> {
+    if(err)
+    console.log(err);
+    else
+    console.log(JSON.stringify(res, null, 2))
+})
+
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
-
-
 //Start the server
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
@@ -21,6 +44,7 @@ app.get('/', (req,res)=>{
     res.send("Hello");
 })
 
+//rout to /views/test.html when user go to localhost:5000/main
 app.get('/main', (req,res)=>{
     res.sendFile('test.html', {root: path.join(__dirname, 'views')});
 })
