5ee4a0781317c895a3be1532d689eb4d18b96f0f
kev-th <kevinkthaw@gmail.com>
2018-04-29 12:38:10 -0400

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 const path = require('path');
 const fs = require('fs');
 const firebase_route = require ('./routers/firebase');
